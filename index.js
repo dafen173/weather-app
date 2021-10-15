e54032f2bb7ddbf45c2b1f6311fd4dc5709bdce3
alexandr nikandrov <dafen@ukr.net>
2021-10-15 14:10:52 +0300

    finished v.10

--- index.js
+++ index.js
@@ -1,76 +1,119 @@
-// Start from here
+import { weatherByCity } from './fixtures';
 
-const KELVIN_UNITS = 'K';
-const CELSIUS_UNITS = 'C';
-const FAHRENHEIT_UNITS = 'F';
-
-
+import {
+  CELSIUS_UNITS,
+  displayInUnits,
+  FAHRENHEIT_UNITS,
+  getDateFromUnixTimestamp,
+  getIconFromCode,
+} from './utils';
 
 window.dataStore = {
-	currentUnits: CELSIUS_UNITS,
-	currentCity: '',
+  currentUnits: CELSIUS_UNITS,
+  currentCity: '',
 };
 
-
+function setCurrentUnits(value) {
+  window.dataStore.currentUnits = value;
+  window.renderApp();
+}
 
 window.renderApp = renderApp;
+
 renderApp();
-//document.getElementById('app-root').innerHTML = App();
-function renderApp () {
-	document.getElementById('app-root').innerHTML = `
+function renderApp() {
+  document.getElementById('app-root').innerHTML = `
 		${App()}
 	`;
 }
 
-
 function App() {
-	return `<div>
+  return `<div>
 		${SearchByCity()}
-		${UnitSwitch(window.dataStore.currentUnits, () => {})}
+		${UnitSwitch(window.dataStore.currentUnits, setCurrentUnits)}
 		<br/>
 		${WeatherToday()}
 		<br/>
 		${WeatherForecast()}
-	</div>`
+	</div>`;
 }
-
 
 function SearchByCity() {
-	return `<input
+  const weatherData = weatherByCity[window.dataStore.currentCity];
+
+  return `<input
 		type='text'
 		value="${window.dataStore.currentCity}"
-		onchange="window.dataStore.currentCity = this.value; window.renderApp();" />`;
+		onchange="window.dataStore.currentCity = this.value; window.renderApp()" />
+		${!weatherData ? `Enter one of the city names: ${Object.keys(weatherByCity)}` : ''}`;
 }
 
-function UnitSwitch(currentUnits, setCurrentUnits) {
-	return `<p>Select units</p>
-	${[{value: CELSIUS_UNITS}, {value: FAHRENHEIT_UNITS}].map}
-	<div>
-		<input 
-		type="radio" 
-		value="" 
-		checked
-		onchange="(${setCurrentUnits})(this.value);"
-		/>
-	</div>
-
-
-
+function UnitSwitch(currentUnits, setCurrentUnitsCB) {
+  return `<p>Select units</p>
+		${[
+      { id: 'celsius-units', value: CELSIUS_UNITS, name: 'C' },
+      { id: 'fahrenheit-units', value: FAHRENHEIT_UNITS, name: 'F' },
+    ]
+      .map(
+        ({ id, value, name }) =>
+          `<div>
+						<input
+							id="${id}"
+							type="radio" 
+							value="${value}"
+							${currentUnits === value ? 'checked' : ''} 							
+							onchange="(${setCurrentUnitsCB})(this.value);"
+						/>
+						<label for = "id">"${name}"</label>
+					</div>`,
+      )
+      .join('')}
 	`;
 }
 
 function WeatherToday() {
-	return `WeatherToday ${window.dataStore.currentCity}`;
+  const { currentCity, currentUnits } = window.dataStore;
+  const weatherData = weatherByCity[currentCity];
+
+  let content = '';
+
+  if (weatherData) {
+    const {
+      current: {
+        dt,
+        temp,
+        weather: [{ main, description, icon }],
+      },
+    } = weatherData;
+
+    const currentDate = getDateFromUnixTimestamp(dt);
+    const weatherIcon = getIconFromCode(icon);
+    const tempInUnits = displayInUnits(temp, currentUnits);
+    content += `<div>Weather for ${currentDate} in ${currentCity}</div>`;
+    content += `<div>${weatherIcon} ${main} (${description}). Temperature is ${tempInUnits}</div>`;
+  }
+  return content ? `<div>${content}</div>` : '';
 }
 
 function WeatherForecast() {
-	return 'WeatherForecast';
+  const { currentCity, currentUnits } = window.dataStore;
+  const weatherData = weatherByCity[currentCity];
+  let content = '';
+
+  if (weatherData) {
+    content += `Weather forcast for ${currentCity}:`;
+    const { daily } = weatherData;
+    content += daily
+      .slice(1)
+      .map(({ dt, temp: { day, night }, weather: [{ main, description, icon }] }) => {
+        const dateString = getDateFromUnixTimestamp(dt);
+        const dayTempInUnits = displayInUnits(day, currentUnits);
+        const nightTempInUnits = displayInUnits(night, currentUnits);
+        const weatherIcon = getIconFromCode(icon);
+        return `<div>For ${dateString}, ${weatherIcon} ${main} (${description}). Day at ${dayTempInUnits}, night at ${nightTempInUnits}</div>`;
+      })
+      .join('');
+  }
+
+  return content ? `<div>${content}</div>` : '';
 }
-
-
-
-
-
-
-
-

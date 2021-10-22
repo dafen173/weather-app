9d951753caab5fc9d7b14b4e0f5b2e659bbf4774
Alex <dafen@ukr.net>
2021-10-22 20:55:13 +0300

    fix error

--- index.js
+++ index.js
@@ -1,76 +1,35 @@
-import { weatherByCity } from './fixtures';
-
 import {
+  allowedCities,
   CELSIUS_UNITS,
   displayInUnits,
   FAHRENHEIT_UNITS,
   getDateFromUnixTimestamp,
   getIconFromCode,
+  getOpenWeatherMapUrl,
 } from './utils';
 
+if (module.hot) {
+  module.hot.accept();
+}
+
 window.dataStore = {
-  currentUnits: CELSIUS_UNITS,
   currentCity: '',
   isDataLoading: false,
   error: null,
   cityByWeather: {},
+  currentUnits: CELSIUS_UNITS,
 };
-
-function setCurrentUnits(value) {
-  window.dataStore.currentUnits = value;
-  window.renderApp();
-}
 
 window.renderApp = renderApp;
 window.performSearch = performSearch;
 window.validateAndLoadData = validateAndLoadData;
 
-renderApp();
-function renderApp() {
-  document.getElementById('app-root').innerHTML = `
-		${App()}
-	`;
-}
-
-function weatherResults() {
-  const { currentCity } = window.dataStore;
-
-  let content = '';
-  //initial state
-  if (currentUnits === '') {
-    content = 'Search by city name';
-  } else {
-    //error state
-    //loading state
-    //results state
-    if (isDataLoading) {
-      content = 'Loading...';
-    }
-    if (error !== null) {
-      content = error;
-    }
-    if (isCurrentCityDataLoaded()) {
-      content = `
-			${UnitSwitch(currentUnits, setCurrentUnits)}
-			<br/> 
-			${WeatherToday()}
-			<br/>
-			${WeatherForecast()}
-			`;
-    }
-  }
-  return `<p>${content}</p>`;
-}
-
-function App() {
-  return `<div>
-		${SearchByCity()}
-		${weatherResults}
-	</div>`;
-}
+const setCurrentUnits = function (value) {
+  window.dataStore.currentUnits = value;
+  window.renderApp();
+};
 
 function isCurrentCityDataLoaded() {
-  const { cityByWeather, currentCity } = window.data;
   return Boolean(getCurrentCityData());
 }
 
@@ -78,12 +37,11 @@ function validateAndLoadData() {
   const { currentCity } = window.dataStore;
 
   if (!allowedCities.includes(currentCity)) {
-    const error = `Enter one of the city name: ${allowedCities.join(', ')}.`;
+    const error = `Enter one of the city names: ${allowedCities.join(', ')}.`;
     return Promise.resolve({ error });
   }
 
   const url = getOpenWeatherMapUrl(currentCity);
-
   if (!isCurrentCityDataLoaded()) {
     return fetch(url)
       .then(response => response.json())
@@ -115,37 +73,84 @@ function performSearch(cityName) {
     .finally(window.renderApp);
 }
 
-function SearchByCity() {
-  //const weatherData = weatherByCity[window.dataStore.currentCity];
-  return `<input
-		type='text'
-		value="${window.dataStore.currentCity}"
-		onchange="window.performSearch(this.value)" 
-		/>`;
-  //${!weatherData ? `Enter one of the city names: ${Object.keys(weatherByCity)}` : ''}`;
+renderApp();
+
+function renderApp() {
+  document.getElementById('app-root').innerHTML = `
+        ${App()}
+    `;
+}
+
+function WeatherResults() {
+  const { currentCity, isDataLoading, error, currentUnits } = window.dataStore;
+
+  let content = '';
+  if (currentCity === '') {
+    content = 'Search by city name';
+  } else {
+    if (isDataLoading) {
+      content = 'Loading...';
+    }
+
+    if (error !== null) {
+      content = error;
+    }
+
+    if (isCurrentCityDataLoaded()) {
+      content = `
+         ${UnitSwitch(currentUnits, setCurrentUnits)}
+         <br/> 
+         ${WeatherToday()}
+         <br/>
+         ${WeatherForecast()}
+      `;
+    }
+  }
+
+  // console.log({ content });
+  return `<p>${content}</p>`;
+}
+
+function App() {
+  return `<div>
+   ${SearchByCity()}
+   ${WeatherResults()}
+  </div>`;
 }
 
 function UnitSwitch(currentUnits, setCurrentUnitsCB) {
-  return `<p>Select units</p>
-		${[
-      { id: 'celsius-units', value: CELSIUS_UNITS, name: 'C' },
-      { id: 'fahrenheit-units', value: FAHRENHEIT_UNITS, name: 'F' },
-    ]
-      .map(
-        ({ id, value, name }) =>
-          `<div>
-						<input
-							id="${id}"
-							type="radio" 
-							value="${value}"
-							${currentUnits === value ? 'checked' : ''} 							
-							onchange="(${setCurrentUnitsCB})(this.value);"
-						/>
-						<label for = "id">"${name}"</label>
-					</div>`,
-      )
-      .join('')}
-	`;
+  return `
+    <p>Select units</p>
+  ${[
+    { id: 'celsius-units', value: CELSIUS_UNITS, name: 'C' },
+    { id: 'fahrenheit-units', value: FAHRENHEIT_UNITS, name: 'F' },
+  ]
+    .map(
+      ({ id, value, name }) =>
+        `<div>
+          <input 
+              type="radio" 
+              id="${id}"
+              name="temperature-units" 
+              value="${value}" 
+              ${currentUnits === value ? ' checked ' : ''} 
+              onchange="(${setCurrentUnitsCB})(this.value);"
+          >
+            <label for="${id}">˚${name}</label>
+        </div>`,
+    )
+    .join('')}
+`;
+}
+
+function SearchByCity() {
+  return `
+    <input
+        type="text"
+        value="${window.dataStore.currentCity}"
+        onchange="window.performSearch(this.value);" 
+    />
+  `;
 }
 
 function getCurrentCityData() {
@@ -156,7 +161,6 @@ function getCurrentCityData() {
 function WeatherToday() {
   const { currentCity, currentUnits } = window.dataStore;
   const weatherData = getCurrentCityData();
-
   let content = '';
 
   if (weatherData) {
@@ -167,13 +171,13 @@ function WeatherToday() {
         weather: [{ main, description, icon }],
       },
     } = weatherData;
-
-    const currentDate = getDateFromUnixTimestamp(dt);
-    const weatherIcon = getIconFromCode(icon);
     const tempInUnits = displayInUnits(temp, currentUnits);
-    content += `<div>Weather for ${currentDate} in ${currentCity}</div>`;
+    const dateString = getDateFromUnixTimestamp(dt);
+    const weatherIcon = getIconFromCode(icon);
+    content += `<div>Weather for ${dateString} in ${currentCity}:</div>`;
     content += `<div>${weatherIcon} ${main} (${description}). Temperature is ${tempInUnits}</div>`;
   }
+
   return content ? `<div>${content}</div>` : '';
 }
 

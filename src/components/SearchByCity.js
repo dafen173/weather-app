5e209797faeafd176c105465de607fddba66a8a9
Alex <dafen@ukr.net>
2021-11-19 15:32:53 +0200

    setup pragmas

--- src/components/SearchByCity.js
+++ src/components/SearchByCity.js
@@ -1,13 +1,14 @@
 /** @jsx createElement */
 /** @jsxFrag createFragment */
 import { createElement } from '../framework/element';
+import performSearch from '../data/weatherData';
 
 function SearchByCity() {
   return (
     <input
       type="text"
       value={window.dataStore.currentCity}
-      onChange={e => window.performSearch(e.target.value)}
+      onchange={e => performSearch(e.target.value)}
     />
   );
 }

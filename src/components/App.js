43188a505c74d8d3c56646e2aa1db7530d7d9738
Alex <dafen@ukr.net>
2021-11-25 12:11:17 +0200

    Add styled checkbox component

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,28 @@
 /** @jsx createElement */
 /** @jsxFrag createFragment */
 import { createElement, createFragment } from '../framework/element';
+//import Checkbox from './Checkbox/Checkbox';
 import SearchByCity from './SearchByCity';
 import WeatherResults from './WeatherResults';
+import Checkbox from './Checkbox';
 
 function App() {
   return (
     <>
       <SearchByCity />
+      <Checkbox
+        label="next days forecast - at noon only"
+        onChange={e => setForcastPeriodicity(e.target.value)}
+      />
       <WeatherResults />
     </>
   );
+}
+
+function setForcastPeriodicity(isAtNoonOnly) {
+  window.dataStore.isAtNoonOnly = isAtNoonOnly;
+
+  //console.log(window.dataStore);
 }
 
 export default App;

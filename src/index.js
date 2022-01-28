d8d7c10c9a4c25394e09a255a45a82762365984c
Alex <dafen@ukr.net>
2022-01-28 13:38:32 +0200

    try fetch a ktkstock

--- src/index.js
+++ src/index.js
@@ -24,11 +24,10 @@ var selectedFile;
       .addEventListener("change", function(event) {
         selectedFile = event.target.files[0];
       });
-    document
+    
+      /* document
       .getElementById("uploadExcel")
       .addEventListener("click", function() {
-
-        console.log('bababababababababa')
         
         if (selectedFile) {
           console.log("hi");
@@ -45,13 +44,62 @@ var selectedFile;
               );
               let jsonObject = JSON.stringify(rowObject);
               document.getElementById("jsonData").innerHTML = jsonObject;
-              console.log(jsonObject);
+              
             });
           };
           fileReader.readAsBinaryString(selectedFile);
         }
+      }); */
+
+
+
+let res = fetch('http://www.ctccapital.ua/price_a/inetmag/ktkstock.xls',
+      {
+       mode: 'no-cors', 
+      })
+      .then(response => response.text())
+      .then(data => console.log(data))
+
+
+
+
+      document
+      .getElementById("uploadProjectors")
+      .addEventListener("click", function() {
+        if (res) {
+          console.log("hi res");
+          var fileReader = new FileReader();
+          fileReader.onload = function(event) {
+            var data = event.target.result;
+      
+            var workbook = XLSX.read(data, {
+              type: "binary"
+            });
+            workbook.SheetNames.forEach(sheet => {
+              let rowObject = XLSX.utils.sheet_to_row_object_array(
+                workbook.Sheets[sheet]
+              );
+              let jsonObject = JSON.stringify(rowObject);
+              document.getElementById("jsonData").innerHTML = jsonObject;        
+            });
+          };
+          fileReader.readAsBinaryString(res);
+        }
       });
 
+
+
+    
+
+
+
+
+
+//,
+  /* "dependencies": {
+    "read-excel-file": "^5.2.25",
+    "xlsx": "^0.17.5"
+  } */
 
 
 

9dbdaaaaa00c1fa18a8d53a65ba4a6b325c75b5b
Alex <dafen@ukr.net>
2022-02-18 12:48:38 +0200

    set up fetch to google

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ window.validateAndLoadData = validateAndLoadData;
 
 renderApp(App, document.getElementById('app-root'));
 
-
+//==============================================================================================
 
 
 var selectedFile;
@@ -25,7 +25,7 @@ var selectedFile;
         selectedFile = event.target.files[0];
       });
     
-      /* document
+    document
       .getElementById("uploadExcel")
       .addEventListener("click", function() {
         
@@ -49,50 +49,103 @@ var selectedFile;
           };
           fileReader.readAsBinaryString(selectedFile);
         }
-      }); */
+      });
+
+//===============================================================================================
+
+//http://www.ctccapital.ua/price_a/inetmag/ktkstock.xls
+//https://oss.sheetjs.com/test_files/formula_stress_test.xlsx
 
 
 
-let res = fetch('http://www.ctccapital.ua/price_a/inetmag/ktkstock.xls',
+
+/* fetch('https://docs.google.com/spreadsheets/d/1p1Nt_OhXd4NmEEfRGDP91ZzEHKj5ePVN/edit?usp=sharing&ouid=109992617196316266846&rtpof=true&sd=true')
+.then(function(res) {
+    if(!res.ok) throw new Error("fetch failed");
+    return res.arrayBuffer();
+  })  
+.then(function(ab) {
+    var data = new Uint8Array(ab);
+    var workbook = XLSX.read(data, {type:"array"});
+    
+    var first_sheet_name = workbook.SheetNames[0];
+    var worksheet = workbook.Sheets[first_sheet_name];
+    console.log(XLSX.utils.sheet_to_json(worksheet,{raw:true}));
+  }); */
+
+
+
+  document
+  .getElementById("uploadProjectors")
+  .addEventListener("click", function() {
+    console.log('projectors works')
+    fetch('https://docs.google.com/spreadsheets/d/1p1Nt_OhXd4NmEEfRGDP91ZzEHKj5ePVN/edit?usp=sharing&ouid=109992617196316266846&rtpof=true&sd=true')
+    .then(function(res) {
+        /* get the data as a Blob */
+        if(!res.ok) throw new Error("fetch failed")
+        return res.arrayBuffer();
+    })  
+    .then(function(ab) {
+        /* parse the data when it is received */
+        var data = new Uint8Array(ab)
+        var workbook = XLSX.read(data, {type:"array"})
+        
+        /* DO SOMETHING WITH workbook HERE */
+
+        var first_sheet_name = workbook.SheetNames[0]
+        /* Get worksheet */
+        var worksheet = workbook.Sheets[first_sheet_name]
+        var worksheetArray = XLSX.utils.sheet_to_json(worksheet,{raw:true})
+        //console.log(worksheetArray)
+
+        console.log(worksheetArray.map( function(item) {
+          if (item.A === 'Проекторы') {
+            return item.C
+          }
+        }))
+
+        //worksheetArray.forEach( item => console.log(item.J) );
+
+        //let worksheetObject = JSON.stringify(worksheetArray);
+
+
+      
+
+        document.getElementById("jsonData").innerHTML = worksheetArray.map( function(item) {
+          return item.C
+        })
+
+
+
+
+
+    });
+  })
+
+
+
+
+ /*  document
+  .getElementById("uploadProjectors")
+  .addEventListener("click", fetchData())  */
+
+
+
+  //'http://oss.sheetjs.com/test_files/formula_stress_test.xlsx'
+
+
+
+/* let res = fetch('http://www.ctccapital.ua/price_a/inetmag/ktkstock.xls',
       {
        mode: 'no-cors', 
       })
       .then(response => response.text())
-      .then(data => console.log(data))
+      .then(data => console.log(data)) */
 
 
 
 
-      document
-      .getElementById("uploadProjectors")
-      .addEventListener("click", function() {
-        if (res) {
-          console.log("hi res");
-          var fileReader = new FileReader();
-          fileReader.onload = function(event) {
-            var data = event.target.result;
-      
-            var workbook = XLSX.read(data, {
-              type: "binary"
-            });
-            workbook.SheetNames.forEach(sheet => {
-              let rowObject = XLSX.utils.sheet_to_row_object_array(
-                workbook.Sheets[sheet]
-              );
-              let jsonObject = JSON.stringify(rowObject);
-              document.getElementById("jsonData").innerHTML = jsonObject;        
-            });
-          };
-          fileReader.readAsBinaryString(res);
-        }
-      });
-
-
-
-    
-
-
-
+ 
 
 
 //,
@@ -131,7 +184,5 @@ let res = fetch('http://www.ctccapital.ua/price_a/inetmag/ktkstock.xls',
   // `rows` is an array of rows
   // each row being an array of cells.
 //})
-
-
 
 
